0d2c3987f564dc572eddbf34a431df97a7ab5490
phiona-b3 <phiona030303@gmail.com>
2023-11-24 17:30:08 +0300

    code for marketplace page

---
+++ bidofa_marketplace/src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div className="dropdown">
+        <button className="dropdown_section">
+            All Categories
+        </button>
+    </div>
+  )
+}
+
+export default Categories
