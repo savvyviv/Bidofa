4cd597d67f343bbdd40a5d2c75b808c7a24fe84f
phiona-b3 <phiona030303@gmail.com>
2023-11-24 23:57:55 +0300

    footer elements

--- bidofa_marketplace/src/App.js
+++ bidofa_marketplace/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Categories from './components/Categories';
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Categories/>
+      <Footer />
     </>
   )
 }
