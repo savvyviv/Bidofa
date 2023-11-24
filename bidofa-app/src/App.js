09c0b037edf54788329528971b45611432841e65
shakiogunlesi <shaki_ogunlesi@outlook.com>
2023-11-24 08:48:28 +0100

    updated

--- bidofa-app/src/App.js
+++ bidofa-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/LandingPage.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' exact Component= {Home} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
---
+++ bidofa-app/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Home() {
+    return (
+      <>
+        <p>We are coming up with something new</p>
+      </>
+    );
+};
+
+export default Home;
