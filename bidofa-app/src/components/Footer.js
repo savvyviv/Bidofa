7c1ef938e901e48ecf2416a943aad754afc8ed98
shakiogunlesi <shaki_ogunlesi@outlook.com>
2023-11-28 14:43:20 +0100

    Updated landing Page for completion

--- bidofa-app/src/components/Footer.js
+++ bidofa-app/src/components/Footer.js
@@ -12,60 +12,62 @@ function Footer () {
     <div>
       <div className="footer-section">
         <div className="row">
-            <div className="col-sm-3 col-xs-6">
-                <h1>BIDOFA</h1>
-                <p>An exclusive Hub and Marketplace
-                  <br />to exhibit various Digital assets</p>
-            </div>
-            <div className="col-sm-3 col-xs-6"> 
-              <h6>Explore</h6>
-              <h6>Help Center</h6>
-              <h6>Learn</h6>
-              <h6>Become a Partner</h6>
-            </div>
-            <div className="col-sm-3 col-xs-6">
+          <div className="col-md-4 col-xs-12">
+            <h1 className='bodofa-text'>BIDOFA</h1>
+            <h5>An exclusive Hub and Marketplace
+            <br />to exhibit various Digital assets</h5>
+          </div>
+          <div className='col-sm-8 col-xs-12'>
+            <div className='row'>
+              <div className="col-sm-4"> 
+                <h6>Explore</h6>
+                <h6>Help Center</h6>
+                <h6>Learn</h6>
+                <h6>Become a Partner</h6>
+              </div>
+              <div className="col-sm-4">
                 <h6>Blog</h6>
                 <h6>Bidofa token</h6>
                 <h6>Suggest feature</h6>
                 <h6>Contact us</h6>
-            </div>
+              </div>
 
-              <div className="col-sm-3 col-xs-6">
+              <div className="col-sm-4">
                 <h6>Follow us on</h6>
                 <a href="https://www.facbook.com/c/jamesqquick" className="youtube_social">
-                    <FontAwesomeIcon icon={faFacebook} size="2px"/>
+                  <FontAwesomeIcon icon={faFacebook} size="2px"/>
                 </a>
                 <a href="https://www.instagram.com/learnbuildteach"
-                     className="instagram social">
+                  className="instagram social">
                   <FontAwesomeIcon icon={faInstagram} size="2px" />
                 </a>
                 <a href="https://www.twitter.com/jamesqquick" className="twitter social">
                   <FontAwesomeIcon icon={faXTwitter} size="2px" />
                 </a>
               </div>
-              <div className="horizontal">
-                <hr />
-              </div>
-              <div className="footer_items">
-                <div className="row">
-                    <div className="col-sm-4 col-xs-12">
-                      <div className="copyright">
-                        <p>Copyright 2022 Jaja, All rights reserved</p>
-                      </div>
-                    </div>
-                    <div className="col-sm-4 col-xs-12">
-                        <div className="terms"><p>Terms</p></div>
-                       
-                    </div>
-                    <div className="col-sm-4 col-xs-12">
-                        <div className="policy"><p>Privacy Policy</p></div>
-                        
-                    </div>
+            </div>
+          </div>
+          <div className="horizontal">
+            <hr />
+          </div>
+          <div className="footer_items">
+            <div className="row">
+              <div className="col-sm-4 col-xs-12">
+                <div className="copyright">
+                  <h6>Copyright 2022 Jaja, All rights reserved</h6>
                 </div>
               </div>
+              <div className="col-sm-4 col-xs-12">
+                <div className="terms"><h6>Terms</h6></div>
+              </div>
+              <div className="col-sm-4 col-xs-12">
+                <div className="policy"><h6>Privacy Policy</h6></div>
+              </div>
             </div>
+          </div>
         </div>
-        </div>
+      </div>
+    </div>
         
 
   );
